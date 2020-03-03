2cad9d0e7777df210fa24ffbcb594502c88b6a50
Cyoo9 <tyo002@ucr.edu>
2020-03-03 11:34:05 -0800

    new files

---
+++ rand.hpp
@@ -0,0 +1,23 @@
+#ifndef _RAND_HPP_
+#define _RAND_HPP_
+
+#include "base.hpp"
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+class Rand : public Base {
+	public:
+		Rand(){ this->randnum = rand()%100;}
+		virtual double evaluate()  { return randnum; }
+		virtual string stringify() { return to_string(randnum); }
+		Iterator* create_iterator() { }              
+	private:
+	double	randnum;
+
+
+
+};
+#endif
+
