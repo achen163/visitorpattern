89c2e0d6f31e3c8eb8edf995c5132a98e06bfd3d
Cyoo9 <tyo002@ucr.edu>
2020-03-03 13:56:40 -0800

    modified files

--- op.hpp
+++ op.hpp
@@ -7,8 +7,8 @@ class Op : public Base {
     public:
         Op(double value) : Base() {this->value = value; }
         virtual double evaluate() { return value; }
-        virtual std::string stringify() { return to_string(value); }
-	virtual Iterator* create_iterator() { return new NullIterator(new Op(this); }
+        virtual std::string stringify() { return std::to_string(value); }
+	virtual Iterator* create_iterator() { return new NullIterator(this); }
 	virtual Base* get_left() { return nullptr; }
 	virtual Base* get_right() { return nullptr; }
     private:
--- unit_test.cpp
+++ unit_test.cpp
@@ -1,9 +1,29 @@
 #include "gtest/gtest.h"
 #include "iterator.hpp"
-
+#include "op.hpp"
+#include "add.hpp"
+#include "decorator.hpp"
+#include "mult.hpp"
+#include "Floor.hpp"
 int main(int argc, char **argv) {
    ::testing::InitGoogleTest(&argc, argv);
    return RUN_ALL_TESTS();
+}
+
+TEST(BinaryIteratorTest, TestAddBinary) {
+	Base* seven = new Op(7);
+	Base* eight = new Op(8);
+	Base* add = new Add(seven, eight);
+	EXPECT_EQ(add->create_iterator()->current(), seven);
+	EXPECT_EQ(add->create_iterator()->current(), eight);
+}	
+
+TEST(UnaryIteratortest, TestFloorBinary) {
+	Base* two = new Op(2.5);
+	Base* three = new Op(3);
+	Base* mult = new Mult(two, three);
+	Decorator* floor = new Floor(mult);
+	EXPECT_EQ(floor->create_iterator()->current(), mult);
 }
 
 
