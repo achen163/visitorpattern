5f55130e74259b5fc12fd5427064c3d56ae49d41
achen163 <achen163@ucr.edu>
2020-03-03 13:26:07 -0800

    Add file

--- add.hpp
+++ add.hpp
@@ -7,7 +7,7 @@ class Add : public Base {
 	public:
 		Add(Base* A, Base* B) : Base() { this->A = A; this->B = B; }
 		virtual double evaluate() { return A->evaluate() + B->evaluate(); }
-		Iterator* create_iterator() {
+		virtual Iterator* create_iterator() {
 			 return new BinaryIterator(this);
 		}
 		virtual Base* get_left() { return A; }
